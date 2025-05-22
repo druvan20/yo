c550d50ede782a93f7c6789654ab6ea5b89e394a
druvan20 <druvannaveen1234@gmail.com>
2025-05-22 21:05:26 +0530

    ypo

--- add.java
+++ add.java
@@ -1,4 +1,7 @@
-heloo
+public class add{
+	public static void main(String[]args){
+		System.out.world("Hello");
+	}
+}
 
-er,adfnjs
 
--- new/add.java
+++
@@ -1,7 +0,0 @@
-public class add{
-	public static void main(String[]args){
-		System.out.world("Hello");
-	}
-}
-
-
